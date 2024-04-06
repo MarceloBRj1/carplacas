e78d08186066db4dd44f85ab230e923cef9317f0
Marcelo Barreira <131809837+MarceloBRj1@users.noreply.github.com>
2024-04-06 13:46:24 +0000

    talvez finalizado

--- src/app/components/Duvidas.tsx
+++ src/app/components/Duvidas.tsx
@@ -13,19 +13,31 @@ const Duvidas = () => {
     <section className='bg-white container flex-block mb-5 '>
       <h1 className='text-4xl font-bold text-center mt-10 text-orange-500'>Dúvidas frequentes</h1>
       <Accordion type="single" collapsible className='lg:mx-10 mx-0 md:mx-10 my-10 self-center'>
+        <AccordionItem value="item-0">
+          <AccordionTrigger className='text-xl text-blue-900 lg:ml-20'>Como solicitar a placa Mercosul?</AccordionTrigger>
+            <AccordionContent className=' text-center lg:text-left lg:ml-14'>
+              <p className='text-black text-xl lg:ml-6 lg:mr-2'>• Acesse o site do Detran - CE</p>
+              <p className='text-black text-xl lg:ml-6 lg:mr-2'>• Solicite a emissão de um novo Certificado de Registro do Veículo</p>
+              <p className='text-black text-xl lg:ml-6 lg:mr-2'>• Leve seu veículo para a vistoria automotiva, ela é necessária para liberar o novo CRV</p>
+              <p className='text-black text-xl lg:ml-6 lg:mr-2'>• Com a guia de autorização + CNH ou RG em mãos, o proprietário do veículo ira se dirigir até alguma de nossas lojas.</p>
+            </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-2">
+          <AccordionTrigger className='text-xl text-blue-900 lg:ml-20'>Quando é necessário a troca de Placas?</AccordionTrigger>
+            <AccordionContent className=' text-center lg:text-left lg:ml-14 block lg:flex '>
+              <p className='text-black text-xl lg:ml-6 lg:mr-2'>• Veículos em circulação: 
+              <span>
+                <br /> Troca de município e/ou estado. <br /> Se as placas forem furtadas. <br /> Se as placas forem danificadas.<br />Se o veículo mudar de categoria.
+              </span>
+              </p>
+              <p className='text-black text-xl lg:ml-6 lg:mr-2'>• Veículos novos: <br /> Primeiro emplacamento.</p>
+            </AccordionContent>
+        </AccordionItem>
         <AccordionItem value="item-1">
           <AccordionTrigger className='text-xl text-blue-900 lg:ml-20'>O que é preciso para trocar a placa?</AccordionTrigger>
           <AccordionContent className=' text-center lg:text-left lg:ml-14'>
             <p className='text-black text-xl lg:ml-6 lg:mr-2'>
             Com a autorização de estampagem em mãos, é necessário que o proprietário do veículo esteja no local para fazer a troca da placa com o seu documento, seja CNH ou RG.
-            </p>
-          </AccordionContent>
-        </AccordionItem>
-        <AccordionItem value="item-2">
-          <AccordionTrigger className='text-xl text-blue-900 lg:ml-20'>Quando é necessário fazer a troca de placas?</AccordionTrigger>
-          <AccordionContent className=' text-center lg:text-left lg:ml-14'>
-            <p className='text-black text-xl lg:ml-6 lg:mr-2'>
-            A troca da placa pode ser feita de forma voluntária pelo proprietário do carro, em caso de perca ou roubo, avaria da placa antiga do veículo e transferência do veículo
             </p>
           </AccordionContent>
         </AccordionItem>
--- src/app/components/price-section.tsx
+++ src/app/components/price-section.tsx
@@ -8,7 +8,11 @@ import { Separator } from '@/components/ui/separator'
 const PriceSection = () => {
   return (
 <div className="bg-white -mt-24 py-24 sm:py-32">
-  <div className="mx-auto max-w-7xl px-6 lg:px-8">
+  <motion.div
+    initial={{ opacity: 0, y: -30 }}
+    whileInView={{ opacity: 1, y: 0 }}
+    transition={{ duration: 0.5 }}
+  className="mx-auto max-w-7xl px-6 lg:px-8">
     <div className="mx-auto max-w-4xl text-center">
       <p className="mt-2 text-4xl font-bold tracking-tight text-blue-950 sm:text-5xl">Conheça os nossos Produtos
       </p>
@@ -19,56 +23,56 @@ const PriceSection = () => {
       <div className="rounded-3xl p-8 ring-1 xl:p-10 ring-blue-900">
         <h3 id="tier-freelancer" className="text-lg font-semibold leading-8 text-orange-600">Placa para Moto</h3>
         <p className="mt-6 flex items-baseline gap-x-1">
-          <span className="text-4xl font-bold tracking-tight text-orange-500">R$149,99</span>
+          <span className="text-4xl font-bold tracking-tight text-orange-500">R$150</span>
           <span className="text-sm font-semibold leading-6 text-blue-900">à vista</span>
         </p>
-        <a href="#" aria-describedby="tier-freelancer" className="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-900 text-white shadow-sm hover:bg-orange-500 ">Comprar</a>
+        <a target='_blank' href="https://api.whatsapp.com/send?phone=5585996950064&text=Ol%C3%A1%20gostaria%20de%20fazer%20um%20or%C3%A7amento!" aria-describedby="tier-freelancer" className="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-900 text-white shadow-sm hover:bg-orange-500 ">Comprar</a>
         <ul role="list" className="mt-8 space-y-3 text-sm leading-6 xl:mt-10 text-gray-400">
           <li className="flex gap-x-3 text-slate-900 text-lg">
             <svg className="h-6 w-5 flex-none text-yellow-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            PIV no padrão Mercosul com dimensão de 200mm x 170mm para motocicletas.
+            PIV no padrão Mercosul com dimensão de 20cm x 20cm para motocicletas.
           </li>
         </ul>
-        <Image src="/moto2.png" alt="Moto" width={280} height={280} className='ml-2 lg:ml-4 flex' />
+        <Image src="/moto2.png" alt="Moto" width={280} height={280} className='ml-1 lg:ml-4 flex' />
       </div>
       <div className="rounded-3xl p-8 ring-1 xl:p-10 ring-blue-900">
         <h3 id="tier-freelancer" className="text-lg font-semibold leading-8 text-orange-600">Placa para Carro</h3>
         <p className="mt-6 flex items-baseline gap-x-1">
-          <span className="text-4xl font-bold tracking-tight text-orange-500">R$259,99</span>
+          <span className="text-4xl font-bold tracking-tight text-orange-500">R$260</span>
           <span className="text-sm font-semibold leading-6 text-blue-900">à vista</span>
         </p>
-        <a href="#" aria-describedby="tier-freelancer" className="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-900 text-white shadow-sm hover:bg-orange-500 ">Comprar</a>
+        <a target='_blank' href="https://api.whatsapp.com/send?phone=5585996950064&text=Ol%C3%A1%20gostaria%20de%20fazer%20um%20or%C3%A7amento!" aria-describedby="tier-freelancer" className="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-900 text-white shadow-sm hover:bg-orange-500 ">Comprar</a>
         <ul role="list" className="mt-8 space-y-3 text-sm leading-6 xl:mt-10 text-gray-400">
           <li className="flex gap-x-3 text-slate-900 text-lg">
             <svg className="h-6 w-5 flex-none text-yellow-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            PIV no padrão Mercosul, com dimensão de 400mm x 130mm para automóveis em geral.
+            PIV no padrão Mercosul, com dimensão de 40cm x 13cm para automóveis em geral.
           </li>
         </ul>
-        <Image src="/placacarro.png" alt="Moto" width={220} height={280} className='ml-11 lg:ml-10 flex' />
+        <Image src="/placacarro.png" alt="Moto" width={220} height={280} className='ml-9 lg:ml-10 flex' />
       </div>
       <div className="rounded-3xl p-8 ring-1 xl:p-10 ring-blue-900">
-        <h3 id="tier-freelancer" className="text-lg font-semibold leading-8 text-orange-600">Placa para Moto</h3>
+        <h3 id="tier-freelancer" className="text-lg font-semibold leading-8 text-orange-600">Placa Reduzida(Mini)</h3>
         <p className="mt-6 flex items-baseline gap-x-1">
-          <span className="text-4xl font-bold tracking-tight text-orange-500">R$149,99</span>
+          <span className="text-4xl font-bold tracking-tight text-orange-500">R$280</span>
           <span className="text-sm font-semibold leading-6 text-blue-900">à vista</span>
         </p>
-        <a href="#" aria-describedby="tier-freelancer" className="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-900 text-white shadow-sm hover:bg-orange-500 ">Comprar</a>
+        <a target='_blank' href="https://api.whatsapp.com/send?phone=5585996950064&text=Ol%C3%A1%20gostaria%20de%20fazer%20um%20or%C3%A7amento!" aria-describedby="tier-freelancer" className="mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 bg-blue-900 text-white shadow-sm hover:bg-orange-500 ">Comprar</a>
         <ul role="list" className="mt-8 space-y-3 text-sm leading-6 xl:mt-10 text-gray-400">
           <li className="flex gap-x-3 text-slate-900 text-lg">
             <svg className="h-6 w-5 flex-none text-yellow-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
               <path fill-rule="evenodd" d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z" clip-rule="evenodd" />
             </svg>
-            PIV no padrão Mercosul com dimensão de 200mm x 170mm para motocicletas.
+            PIV no padrão Mercosul com dimensão de Mini: 34cm x 11cm para motocicletas.
           </li>
         </ul>
-        <Image src="/moto2.png" alt="Moto" width={280} height={280} className='ml-2 lg:ml-4 flex' />
+        <Image src="/placamini.png" alt="Moto" width={280} height={280} className='ml-2 lg:ml-4 flex' />
       </div>
     </div>
-  </div>
+  </motion.div>
 </div>
 
   )
